ef42d72cb7b10c92ea3c61da33fa6f38cccdcdfa
jiayouxjh <xjhssg@gmail.com>
2015-01-14 17:01:48 +0800

    完善代码

--- OpenGL-Test/main.cpp
+++ OpenGL-Test/main.cpp
@@ -3,13 +3,27 @@
 #include <GLUT/GLUT.h>
 #include "Triangle.h"
 
+#define CHAPTOR_1 1
+#define CHAPTOR_2 2
+
 int main(int argc, char* argv[])
 {
     gltSetWorkingDirectory(argv[0]);
     glutInit(&argc, argv);
     
-    Triangle *triangle = new Triangle();
-    triangle->show();
+    unsigned int chaptor = 1;
+    switch (chaptor) {
+        case CHAPTOR_1:
+        {
+            Triangle *triangle = new Triangle();
+            triangle->show();
+            break;
+        }
+        case CHAPTOR_2:
+        {
+            break;
+        }
+    }
     
     return 0;
 }
--- OpenGL-Test/src/chaptor_01/Triangle.cpp
+++ OpenGL-Test/src/chaptor_01/Triangle.cpp
@@ -70,32 +70,6 @@ void RenderScene(void)
     glutSwapBuffers();
 }
 
-
-///////////////////////////////////////////////////////////////////////////////
-// Main entry point for GLUT based programs
-//int main(int argc, char* argv[])
-//{
-//    gltSetWorkingDirectory(argv[0]);
-//    
-//    glutInit(&argc, argv);
-//    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_STENCIL);
-//    glutInitWindowSize(800, 600);
-//    glutCreateWindow("Triangle");
-//    glutReshapeFunc(ChangeSize);
-//    glutDisplayFunc(RenderScene);
-//    
-//    GLenum err = glewInit();
-//    if (GLEW_OK != err) {
-//        fprintf(stderr, "GLEW Error: %s\n", glewGetErrorString(err));
-//        return 1;
-//    }
-//    
-//    SetupRC();
-//    
-//    glutMainLoop();
-//    return 0;
-//}
-
 Triangle::Triangle()
 {
 
